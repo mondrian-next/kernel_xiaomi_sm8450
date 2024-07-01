a94ea16951b0c0e4d6e30fd54e4d767db8b78742
NextWork123 <nextworks@protonmail.com>
2024-07-01 17:53:35 +0200

    inline.c: fix an error

--- fs/f2fs/inline.c
+++ fs/f2fs/inline.c
@@ -804,7 +804,6 @@ int f2fs_inline_data_fiemap(struct inode *inode,
 	byteaddr += (char *)inline_data_addr(inode, ipage) -
 					(char *)F2FS_INODE(ipage);
 	err = fiemap_fill_next_extent(fieinfo, start, byteaddr, ilen, flags);
-	trace_f2fs_fiemap(inode, start, byteaddr, ilen, flags, err);
 out:
 	f2fs_put_page(ipage, 1);
 	return err;
